ac5a768180124e4f29cc7504b9d8231d8259cbf0
oussamaelhousni <oussamaelhousnioe@gmail.com>
2023-02-28 19:08:03 +0100

    initial commit

---
+++ books/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Book
+# Create your views here.
+
+class BookList(ListView):
+    model = Book
+    template_name = "book_list.html"
